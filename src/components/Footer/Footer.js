f7ef60a99bc2d690d658667018a447de4121c92b
MELEK <100144640+MELEK-GRL@users.noreply.github.com>
2023-03-17 02:57:09 +0300

    tailwind

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <div className='flex items-center justify-center w-full bg-gray-900 p-4'>
+    <div className='flex text-white text-xl items-center justify-center w-full bg-gray-900 p-4'>
       FOOTER
     </div>
   )
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ export default function Layout() {
     return (
         <div className="boxCenter w-full flex-col  ">
             <Header />
-            <div className="flex mb-8 mt-[60px] w-full h-[3513px];  lg:w-[643px] overflow-hiden ">
+            <div className="flex min-h-[569px] max-h-[4000px]  overflow-hidden mb-8 mt-[60px] w-full h-[3513px];  lg:w-[643px] overflow-hiden ">
                 <Routers />
             </div>
             <Footer />
